3db85964d0e8bc5c3586631c7aba4d94a5e406c8
Patty Kovash <pattykovash@gmail.com>
2018-01-14 13:30:55 -0700

    Restore FB Oauth code

--- server/index.js
+++ server/index.js
@@ -1,20 +1,85 @@
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const express = require('express');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const session = require('express-session');
 const db = require('../database/index');
-const mailer = require('../mailer/mailer');
-
+const fbConfig = require('../facebookConfig');
 const port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(cookieParser());
+app.use(session({
+  secret: 'Some Crazy Secret',
+  key: 'legos',
+  saveUninitialized: true,
+  resave: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
+let isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+};
+
+passport.use(new FacebookStrategy(
+  {
+    clientID: fbConfig.clientID,
+    clientSecret: fbConfig.clientSecret,
+    callbackURL: fbConfig.callbackURL,
+    profileFields: fbConfig.profileFields
+  }, (accessToken, refreshToken, profile, done) => {
+    db.User.findOneAndUpdate(
+      { fbId: { $eq: profile.id } },
+      {
+        userName: `${ profile.name.givenName } ${ profile.name.familyName }`,
+        email: profile.emails[0].value,
+        fbId: profile.id,
+        fbAccessToken: accessToken
+      },
+      {
+        upsert: true,
+        new: true,
+        runValidators: true
+      }, (err, user) => {
+        return done(null, user);
+      });
+  }));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((userObj, done) => {
+  done(null, userObj);
+});
 
 app.get('/', (req, res) => {
   res.render('index', { user: req.user });
 });
+
+app.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile', { user: req.user });
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook', {
+  scope: ['email', 'public_profile']
+}));
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
 
 app.get('/listings', (req, res) => {
 
@@ -29,7 +94,7 @@ app.get('/listings', (req, res) => {
 
 });
 
-app.post('/listings', (req, res) => {
+app.post('/listing', (req, res) => {
   db.saveListing(req.body, function(err, data) {
     if (err) {
       res.sendStatus(500);
@@ -37,18 +102,6 @@ app.post('/listings', (req, res) => {
       res.sendStatus(200);
     }
   });
-});
-
-app.post('/mailer', (req, res) => {
-  var name = req.body.name;
-  var email = req.body.email;
-  var message = req.body.message;
-  if (name && email && message) {
-    mailer.sendMail(name, email, message);
-    res.sendStatus(200)
-  } else {
-    res.sendStatus(500)
-  }
 });
 
 app.listen(port, () => {
